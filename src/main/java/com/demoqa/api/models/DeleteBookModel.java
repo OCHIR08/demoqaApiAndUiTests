67fec037f565fdb6e0853423bb074ce52bbb257f
ochirermoshkaev <oermoshkaev@gmail.com>
2026-02-15 03:21:40 +0300

    refactor: Оптимизация базового класса BaseApiTest и исправление авторизации - Исправлен баг инициализации: Изменен порядок выполнения в @BeforeAll. Теперь токен авторизации получается до создания спецификации, что решает проблему ошибки 401 (Unauthorized). - Добавлен RequestSpecification: Все общие параметры запросов (Base URL, Content-Type, Auth Header) вынесены в единую спецификацию requestSpec. - Чистота кода: Убрано дублирование настроек в тестах, теперь авторизация подставляется автоматически через спецификацию.

--- src/main/java/com/demoqa/api/models/DeleteBookModel.java
+++ src/main/java/com/demoqa/api/models/DeleteBookModel.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class DeletBookModel {
+public class DeleteBookModel {
     private String isbn;
     private String userId;
 }
--- src/test/java/com/demoqa/base/BaseApiTest.java
+++ src/test/java/com/demoqa/base/BaseApiTest.java
@@ -3,7 +3,10 @@ package com.demoqa.base;
 import com.demoqa.api.clients.LoginStepsApi;
 import com.demoqa.config.Config;
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeAll;
 
 public class BaseApiTest {
@@ -11,9 +14,13 @@ public class BaseApiTest {
     public static String authToken;
     public static String userId;
 
+    public static RequestSpecification requestSpec;
+
     @BeforeAll
     public static void setup() {
         RestAssured.baseURI = Config.baseUrl();
+
+
 
         final LoginStepsApi loginSteps = new LoginStepsApi();
         String username = Config.loginIvan();
@@ -27,5 +34,12 @@ public class BaseApiTest {
 
         authToken = response.path("token");
         userId = response.path("userId");
+
+        requestSpec = new RequestSpecBuilder()
+                .setBaseUri(Config.baseUrl())
+                .setContentType(ContentType.JSON)
+                .addHeader("Authorization", "Bearer " + authToken)
+                .build();
+
     }
 }
